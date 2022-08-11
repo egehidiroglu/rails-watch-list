3768bd3336646b0e38770108f63632e5bf1d9001
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-11 16:05:48 -0400

    Finished implementing the routes and all the controller methods. Started adding design.

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    list = List.find(params[:list_id])
+    @bookmark.list = list
+    @bookmark.save
+    redirect_to list_path(list)
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    list_id = @bookmark.list_id
+    @bookmark.destroy
+    redirect_to list_path(list_id), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[create index show new] do
     resources :bookmarks, only: %i[create new]
   end
+  resources :bookmarks, only: [:destroy]
 end
