51bc5f73ba7059a22adbef82cad9f16d6e03c6ad
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-11 14:09:13 -0400

    Added lists controller and created the route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
 require 'json'
 require 'open-uri'
 
-buffer = open("https://api.themoviedb.org/3/movie/popular?api_key=63759eccae824fa88e79218786680970&language=en-US&page=1").read
+buffer = URI.open("https://api.themoviedb.org/3/movie/popular?api_key=63759eccae824fa88e79218786680970&language=en-US&page=1").read
 result = JSON.parse(buffer)
-p result
+results = result["results"]
+
+results.each do |movie|
+  Movie.create(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    rating: movie["vote_average"],
+    poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}"
+  )
+end
