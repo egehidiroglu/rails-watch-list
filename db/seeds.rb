77f252d3c9c19d454783cafacb89415a64aab0e7
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-11 13:47:09 -0400

    Added models and created the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+buffer = open("https://api.themoviedb.org/3/movie/popular?api_key=63759eccae824fa88e79218786680970&language=en-US&page=1").read
+result = JSON.parse(buffer)
+p result
